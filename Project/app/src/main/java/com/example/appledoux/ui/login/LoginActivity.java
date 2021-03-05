c4b02044bd5ec89f6df0316fdadc0c9b9d5a2c76
Alexis LEDOUX <alexis.ledoux@edu.devinci.fr>
2021-03-06 00:00:48 +0100

    TIME TO COMMIT

--- Project/app/src/main/java/com/example/appledoux/ui/login/LoginActivity.java
+++ Project/app/src/main/java/com/example/appledoux/ui/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.appledoux.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -21,12 +22,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.example.appledoux.R;
 import com.example.appledoux.data.MaBaseSQLite;
 
@@ -34,6 +41,14 @@ import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import com.androidnetworking.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -52,11 +67,13 @@ public class LoginActivity extends AppCompatActivity {
     private static final String COL_ID = "ID";
     private static final String COL_SALT="SALT";
     private static final int NUM_COL_ID = 0;
+    private static String Iduser = "";
     private MaBaseSQLite maBaseSQLite;
     private SQLiteDatabase bdd;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AndroidNetworking.initialize(getApplicationContext());
         //this.deleteDatabase("app.db3");
         maBaseSQLite = new MaBaseSQLite(this,NOM_BDD,null,VERSION_BDD);
         open();
@@ -81,6 +98,7 @@ public class LoginActivity extends AppCompatActivity {
                     try {
                         MessageDigest md = MessageDigest.getInstance("SHA-512");
                         md.update(salt);
+                        Iduser = IdText.getText().toString();
                         byte[] hash = md.digest(IdText.getText().toString().getBytes(StandardCharsets.UTF_8));
                         values.put(COL_SALT,salt);
                         values.put(COL_ID,hash);
@@ -130,6 +148,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                     if (loginResult.getSuccess() != null) {
                         updateUiWithUser(loginResult.getSuccess());
+                        Iduser = usernameEditText.getText().toString();
                         affichage();
                     }
                     setResult(Activity.RESULT_OK);
@@ -199,17 +218,81 @@ public class LoginActivity extends AppCompatActivity {
     public void updateAPI()
     {
         //TODO:do it
+        AndroidNetworking.get("https://60102f166c21e10017050128.mockapi.io/labbbank/accounts/")
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // do anything with response
+                        bdd.execSQL("delete from "+ TABLE_BANK);
+                        for (int i = 0; i < response.length(); i++)
+                        {
+                            try {
+                                JSONObject obj = response.getJSONObject(i);
+                                if (obj.getString("id").equals(Iduser))
+                                {
+                                    ContentValues values = new ContentValues();
+                                    values.put(COL_ACCNAME,obj.getString("accountName"));
+                                    values.put(COL_AMOUNT,obj.getInt("amount"));
+                                    values.put(COL_IBAN,obj.getString("iban"));
+                                    values.put(COL_CURREN,obj.getString("currency"));
+                                    if (bdd.insert(TABLE_BANK,null,values)==-1)
+                                    {
+                                        return;
+                                    }
+                                }
+                            } catch (JSONException e) {
+                                return;
+                            }
+                        }
+                        updateListView();
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+
+                    }
+                });
+
     }
-    public void affichage()
+    public void updateListView()
     {
-        setContentView(R.layout.affichage);
-        final RecyclerView view = findViewById(R.id.recyclerViewAccount);
-        //TODO:check with database
+        final TextView view = findViewById(R.id.Listaffichage);
         Cursor c = bdd.query(TABLE_BANK,new String[] {COL_ACCNAME,COL_AMOUNT,COL_CURREN,COL_IBAN},null,null,null,null,null);
         if (c.getCount()==0)
         {
 
+            view.setText("Liste Vide, veuillez remplir en actualisant la liste");
+
         }
+        else
+        {
+            String retour="";
+            while(c.moveToNext())
+            {
+                int index;
+
+                index = c.getColumnIndexOrThrow(COL_ACCNAME);
+                String account_name = c.getString(index);
+
+                index = c.getColumnIndexOrThrow(COL_AMOUNT);
+                int amount = c.getInt(index);
+
+                index = c.getColumnIndexOrThrow(COL_IBAN);
+                String iban = c.getString(index);
+                index = c.getColumnIndexOrThrow(COL_CURREN);
+                String currency = c.getString(index);
+                retour += account_name+ " (IBAN: " + iban + "): \r\n" + amount + " " + currency + "\r\n";
+            }
+            view.setText(retour);
+        }
+    }
+    public void affichage()
+    {
+        setContentView(R.layout.affichage);
+
+        //TODO:check with database
+        updateListView();
         final Button updatebutton = findViewById(R.id.buttonupdate);
         updatebutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -225,5 +308,29 @@ public class LoginActivity extends AppCompatActivity {
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
         finish();
+    }
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
